46a2ea4138a998575a052b8c4e2d7341c312587d
Y Lin <linyeah2011@gmail.com>
2021-09-21 15:34:39 +0800

    add exception for encode string

--- helper.py
+++ helper.py
@@ -1,7 +1,9 @@
 import cv2
-import base64
-
+try:
+    from base64 import encodebytes
+except ImportError:
+    from base64 import encodestring as encodebytes
 def image_to_dataURI(image):
     content = cv2.imencode('.png', image)[1].tostring()
-    encoded = base64.encodestring(content)
+    encoded = encodebytes(content)
     return 'data:image/png ;base64, ' + str(encoded, 'utf-8')
